596fc8f59e6c416c73ceecd4afd191bf9f5b5bf6
Miguel Gallegos <miguelgallegosgt@gmail.com>
2022-05-20 16:55:16 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <CampsitesDirectoryPage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
