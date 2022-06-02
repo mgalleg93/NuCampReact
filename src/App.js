cbd3d760d6bf4995e517f7f39468851785606958
Miguel Gallegos <miguelgallegosgt@gmail.com>
2022-06-02 14:04:36 -0700

    Fetch data

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
+import { fetchPartners } from './features/partners/partnersSlice';
+import { fetchPromotions } from './features/promotions/promotionsSlice';
 
 function App() {
 
@@ -18,6 +20,8 @@ function App() {
 
   useEffect(() => {
     dispatch(fetchCampsites());
+    dispatch(fetchPartners());
+    dispatch(fetchPromotions());
   }, [dispatch]);
 
   return (
